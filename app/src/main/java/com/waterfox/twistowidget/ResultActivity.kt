9291821ced60e47185614baf12b5a825360f4672
Waterfox <waterfox80@gmail.com>
2023-12-14 23:41:54 +0100

    Add button from result

--- app/src/main/java/com/waterfox/twistowidget/ResultActivity.kt
+++ app/src/main/java/com/waterfox/twistowidget/ResultActivity.kt
@@ -3,10 +3,14 @@ package com.waterfox.twistowidget
 import android.app.Service
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import com.waterfox.twistowidget.apisearcher.ServiceBuilder
 import com.waterfox.twistowidget.apisearcher.StopService
 import com.waterfox.twistowidget.apisearcher.result.StopResult
@@ -17,53 +21,56 @@ import retrofit2.Response
 
 class ResultActivity : AppCompatActivity()
 {
-    private lateinit var queryString:String;
-    private lateinit var resultList:ListView;
-    private lateinit var stopNames: ArrayList<String>;
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+    private lateinit var queryString: String
+    private lateinit var resultList: ScrollView
+    private lateinit var stopNames: ArrayList<String>
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var linearLayoutResults: LinearLayout
 
-        this.queryString = intent.getStringExtra("QueryString").toString();
-        initComponent();
-        loadResult();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        this.queryString = intent.getStringExtra("QueryString").toString()
+        initComponent()
+        loadResult()
     }
 
     private fun loadResult() {
-        val stopService = ServiceBuilder.buildService(StopService::class.java);
-        val requestCall = stopService.getStops("stop_name like '%${this.queryString}%'", 20);
-        println(requestCall.request().url());
+        val stopService = ServiceBuilder.buildService(StopService::class.java)
+        val requestCall = stopService.getStops("stop_name like '%${this.queryString}%'", 20)
+        println(requestCall.request().url())
         requestCall.enqueue(object : Callback<StopResult> {
             override fun onResponse(call: Call<StopResult>, response: Response<StopResult>) {
                 if (response.isSuccessful) {
-                    val stopResult = response.body();
+                    val stopResult = response.body()
                     if (stopResult != null) {
                         for (r: StopSpecific in stopResult.results) {
-                            stopNames.add(r.stop_name);
+                            val b = Button(this@ResultActivity)
+                            b.text = r.stop_name
+                            linearLayoutResults.addView(b)
                         }
-                        val adapter = ArrayAdapter(this@ResultActivity, android.R.layout.simple_list_item_1, stopNames);
-                        resultList.adapter = adapter
                     }
                 } else {
-                    val text = "Sorry, but the request failed";
+                    val text = "Sorry, but the request failed"
                     val duration = Toast.LENGTH_SHORT
-
-                    val toast = Toast.makeText(this@ResultActivity, text, duration);
-                    toast.show();
+                    val toast = Toast.makeText(this@ResultActivity, text, duration)
+                    toast.show()
                 }
             }
 
             override fun onFailure(call: Call<StopResult>, t: Throwable) {
+                // Handle failure if needed
             }
         })
     }
 
-    private fun initComponent()
-    {
-        this.resultList = findViewById<ListView>(R.id.result_activity_result_list);
-        this.stopNames = ArrayList();
+    private fun initComponent() {
+        this.resultList = findViewById<ScrollView>(R.id.result_scroll_view)
+        this.stopNames = ArrayList()
+        this.linearLayoutResults = findViewById(R.id.linear_layout_results)
     }
 
 }
+
 
